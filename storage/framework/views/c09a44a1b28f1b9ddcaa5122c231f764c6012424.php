ba891a0ccd3881952816ae208ff2173fb211c4ca
martins-2002 <60940110+martins-2002@users.noreply.github.com>
2020-02-12 16:03:31 -0300

    First Commit

---
+++ storage/framework/views/c09a44a1b28f1b9ddcaa5122c231f764c6012424.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Land</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="<?php echo e(asset('vendor/fontawesome-free/css/all.min.css')); ?>" rel="stylesheet">
+  <link href="<?php echo e(asset('vendor/simple-line-icons/css/simple-line-icons.css')); ?>" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+  <!-- Custom styles for this template -->
+  <link href="<?php echo e(asset('css/landing-page.min.css')); ?>" rel="stylesheet">
+
+  <link rel="stylesheet" href="<?php echo e(asset('css/template/index.css')); ?>">
+
+</head>
+
+<body>
+
+     <!-- Navigation -->
+    <nav class="navbar navbar-light bg-light static-top">
+        <div class="container">
+          <a class="navbar-brand" href="#">Finding Car(logo)</a>
+          <a class="btn btn-danger" href=" <?php echo e(route('login')); ?> ">Cadastre-se</a>
+        </div>
+    </nav>
+
+
+    <?php echo $__env->yieldContent('conteudo'); ?>
+
+
+
+    <!-- Bootstrap core JavaScript -->
+   <script src="<?php echo e(asset('vendor/jquery/jquery.min.js')); ?>"></script>
+   <script src="<?php echo e(asset('vendor/bootstrap/js/bootstrap.bundle.min.js')); ?>"></script>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\Finding-Car\O novo pi(q o wellington ajudou)\henricao\findingcar\resources\views/layouts/estruturaPaginaHomeCliente.blade.php ENDPATH**/ ?>
